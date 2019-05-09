ad97a440f0246910aeac868bc5751063b0673a05
Chiraag Gohel <cgohel@gmail.com>
2019-05-09 17:34:26 -0500

    create_training_data(s) method complete

--- baseline.py
+++ baseline.py
@@ -1,30 +1,33 @@
 import argparse
 import numpy as np
 
-
-def get_indices_period(s):
-    return [i for i, c in enumerate(s) if c == "."]
-
 def getindices(s):
-    return [i for i, c in enumerate(s) if c.isupper()]
+    return [i for i, c in enumerate(s) if c == "\t"]
 
-def main():
+def create_training_data(s):
     train_file = open("data/msr_paraphrase_train.txt")
     train = train_file.read().split('\n')
     train = train[1:]
+    print(train[len(train)-1])
 
     train_dict = {}
 
     for s, i in zip(train, range(1, len(train))):
-        upper = getindices(s)
-        period = get_indices_period(s)
+
+        tabs = getindices(s)
         quality = s[0]
-        first = s[15:period[0]]
-        second = s[period[0]+1:]
+        first = s[tabs[2]+1:tabs[3]]
+        second = s[tabs[3]+1:]
         train_dict[i] = {}
         train_dict[i]['quality'] = quality
         train_dict[i]['first'] = first
         train_dict[i]['second'] = second
+
+    return train_dict
+    
+def main():
+    train = create_training_data("data/msr_paraphrase_train.txt")
+
 
 if __name__ == '__main__':
     main()
